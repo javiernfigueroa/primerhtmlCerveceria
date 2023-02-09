3ac21ba70066d183c58f9f9286a41d65b0957391
javiernfigueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-02-08 21:41:50 -0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,7 @@
+
+document.getElementById("login").addEventListener("click", function(){
+    alert("Su mensaje ha sido enviado, pronto nos contactaremos con usted");
+})
+document.getElementById("a").addEventListener("click", function(){
+    alert("Ususario o contraseña invalida");
+})
